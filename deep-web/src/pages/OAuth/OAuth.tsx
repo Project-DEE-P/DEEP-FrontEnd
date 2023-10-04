4a5fb59d71decb3b4f9035600739b2a667c9fec3
유지민 <a01040987587@gmail.com>
2023-10-05 08:58:25 +0900

    :bug: :: 충동해결

--- deep-web/src/pages/OAuth/OAuth.tsx
+++ deep-web/src/pages/OAuth/OAuth.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import OAuth from "../components/auth/index";
+import OAuth from "../../components/auth/index";
 
 export default function OAuthPage() {
   return (
--- deep-web/src/router/router.tsx
+++ deep-web/src/router/router.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ShowCard from "../pages/ShowCard/index";
+import OAuthPage from "../pages/OAuth/OAuth";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="showCard" element={<ShowCard />} />
+        <Route path="/oauth" element={<OAuthPage />} />
       </Routes>
     </BrowserRouter>
   );
