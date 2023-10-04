7ab1a24b1b7773cce5804a626bea243fa8ccc763
유지민 <a01040987587@gmail.com>
2023-10-05 01:37:05 +0900

    :sparkles: :: 명함 NFC 퍼블리싱

--- deep-web/src/container/share/Layout/index.tsx
+++ deep-web/src/container/share/Layout/index.tsx
@@ -22,8 +22,54 @@ const NFCTitle = styled.p`
     font-weight: 700;
 `
 const CardContainer = styled.div`
-    
+    display: flex;
+    justify-content: center;
+    align-items: centet;
+    margin-top: 10%;
+    a {
+        img {
+            height: 190px;
+        }
+    }
 `
+const MemoContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-left: 40px;
+    margin-top: 15%;
+`
+
+const WriteMemo = styled.p`
+    color: var(--gray-300, #8C9AAB);;
+    font-family: Pretendard;
+    font-size: 14px;
+    font-weight: 400;
+`
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 25%;
+`;
+
+const RememberButton = styled.button`
+    width: 328px;
+    height: 56px;
+    flex-shrink: 0;
+    border-radius: 16px;
+    background: var(--blue-400, #4591FA);
+    color: #FFF;
+    font-family: Pretendard;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: none; /* 클릭 효과 제거 */
+    cursor: pointer; /* 포인터 커서 표시 */
+`;
 
 interface Props {
     children: React.ReactNode;
@@ -37,7 +83,19 @@ const Layout = ({ children }: Props) => {
                 <NFCContainer>
                     <NFCTitle>최희건 님의 <span style={{ color: '#1775F9' }}>DEE:P</span> 명함</NFCTitle>
                 </NFCContainer>
-
+                <CardContainer>
+                    <a>
+                        <img src="cardsample.svg" alt="error" />
+                    </a>
+                </CardContainer>
+                <MemoContainer>
+                    최희건님에 대한 메모를 남겨주세요.
+                </MemoContainer>
+                <ButtonContainer>
+                    <RememberButton>
+                        기억하기
+                    </RememberButton>
+                </ButtonContainer>
             </PageContainer>
         </>
     )
