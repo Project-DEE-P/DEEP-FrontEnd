0b3242309590243c762339041b6359164f9ce6ae
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-10-06 08:06:03 +0900

    ⚡️ :: CardList Axios 연결

--- deep-web/src/components/cardList/index.tsx
+++ deep-web/src/components/cardList/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../container/share/Layout";
 import * as S from "./style";
 import AppHeader from "../layout/AppHeader";
 import Card from "./card";
+import axios, { AxiosResponse } from "axios";
+
 const CardList = () => {
+  const [cardData, setCardData] = useState<AxiosResponse | null | void>([]);
+  axios
+    .get("https://127.0.0.1:8080/v1/api/card/remember")
+    .then(function (response) {
+      console.log(response);
+      setCardData(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {});
+
   return (
     <>
       <AppHeader />
