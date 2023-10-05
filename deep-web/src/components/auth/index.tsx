c75acc81be9173bea15608d137eb86b9330d61db
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-10-05 20:38:01 +0900

    ✨ :: OAuth 개발

--- deep-web/src/components/auth/index.tsx
+++ deep-web/src/components/auth/index.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import * as S from "./style";
+import AppHeader from "../layout/AppHeader";
+import DeepLogo from "../../assets/img/DeepLogo.svg";
+import OAuthBtn from "../../assets/img/OAuthBtn.svg";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleOneTapLogin } from "@react-oauth/google";
 
-const OAuth = () => {
+const OAuth: React.FC = () => {
+  const [data, setData] = useState(null);
+
   return (
     <>
-      <div></div>
+      <S.OAuthContainer>
+        <S.DeepLogoImg src={DeepLogo}></S.DeepLogoImg>
+        <S.OAuthButton src={OAuthBtn}></S.OAuthButton>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+          useOneTap
+        />
+      </S.OAuthContainer>
     </>
   );
 };
