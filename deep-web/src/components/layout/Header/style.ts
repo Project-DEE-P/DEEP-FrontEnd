0776a9cfe8a7d4b902094ece5cfbba28f25217fd
유지민 <a01040987587@gmail.com>
2023-11-02 23:41:17 +0900

    :sparkles: :: AppHeader 생성

---
+++ deep-web/src/components/layout/Header/style.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { DefaultBox } from "../../../styles/default";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 40px;
+  background: #fff;
+`;
+
+export const AppHeaderBox = styled(DefaultBox)`
+  position: relative;
+  padding: 0 20px;
+
+  a {
+    img {
+      height: 20px;
+      margin: 10px 0;
+      margin-top: 1.3%;
+    }
+  }
+`;
+
+export const HeaderButtonBox = styled.div`
+  position: absolute;
+  height: 100%;
+  top: 0;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+`;
+
+export const HeaderButton = styled.a`
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+
+//   img {
+//     width: 14px;
+//     height: 14px;
+//     color: #495057;
+//     margin: 8px;
+//   }
+
+  div {
+    color: #0A3570;
+    text-align: center;
+    font-family: 'Pretendard';
+    font-size: 20px;
+    font-weight: 500;
+    margin-left: 50px;
+  }
+
+`;
